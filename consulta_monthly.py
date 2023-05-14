707a6f62dfe09fb89e8b841d1072c9768edf70f7
PeterArgueta <peterarguedo@gmail.com>
2023-05-14 15:25:29 -0600

    new file:   csvpage.py 	new file:   database.csv 	new file:   downloadwithlink.py 	new file:   outputcsv/ALAMEDA ICTA.csv 	new file:   outputcsv/AMATITLAN.csv 	new file:   outputcsv/ANTIGUA GUATEMALA.csv 	new file:   outputcsv/ASUNCION MITA.csv 	new file:   outputcsv/CAMOTAN.csv 	new file:   outputcsv/CATARINA.csv 	new file:   outputcsv/CHAMPERICO FEGUA.csv 	new file:   outputcsv/CHINIQUE.csv 	new file:   outputcsv/CHIXOY PCH.csv 	new file:   outputcsv/COBAN.csv 	new file:   outputcsv/CONCEPCION.csv 	new file:   outputcsv/CUBULCO.csv 	new file:   outputcsv/EL CAPITAN.csv 	new file:   outputcsv/EL TABLON.csv 	new file:   outputcsv/ESQUIPULAS.csv 	new file:   outputcsv/FLORES AEROPUERTO.csv 	new file:   outputcsv/HUEHUETENANGO.csv 	new file:   outputcsv/INSIVUMEH.csv 	new file:   outputcsv/IXCHIGUAN.csv 	new file:   outputcsv/LA AURORA.csv 	new file:   outputcsv/LA FRAGUA.csv 	new file:   outputcsv/LA REFORMA.csv 	new file:   outputcsv/LA UNION.csv 	new file:   outputcsv/LABOR OVALLE.csv 	new file:   outputcsv/LAS VEGAS PHC.csv 	new file:   outputcsv/LO DE COY.csv 	new file:   outputcsv/LOS ALBORES.csv 	new file:   outputcsv/LOS ALTOS.csv 	new file:   outputcsv/LOS ESCLAVOS.csv 	new file:   outputcsv/MARISCOS.csv 	new file:   outputcsv/MAZATENANGO.csv 	new file:   outputcsv/MONTUFAR.csv 	new file:   outputcsv/MORALES MET.csv 	new file:   outputcsv/NEBAJ.csv 	new file:   outputcsv/PACHUTE.csv 	new file:   outputcsv/PANZOS PHC ALTA VERAPAZ.csv 	new file:   outputcsv/PASABIEN.csv 	new file:   outputcsv/PLAYA GRANDE IXCAN.csv 	new file:   outputcsv/POPTUN.csv 	new file:   outputcsv/POTRERO CARRILLO.csv 	new file:   outputcsv/PUERTO BARRIOS PHC.csv 	new file:   outputcsv/QUEZADA.csv 	new file:   outputcsv/RETALHULEU AREOPUERTO.csv 	new file:   outputcsv/SABANA GRANDE.csv 	new file:   outputcsv/SACAPULAS.csv 	new file:   outputcsv/SAN AGUSTIN ACASAGUASTLAN.csv 	new file:   outputcsv/SAN JERONIMO R H.csv 	new file:   outputcsv/SAN JOSE PINULA.csv 	new file:   outputcsv/SAN MARCOS PHC.csv 	new file:   outputcsv/SAN MARTIN JILOTEPEQUE.csv 	new file:   outputcsv/SAN PEDRO AYAMPUC.csv 	new file:   outputcsv/SAN PEDRO NECTA.csv 	new file:   outputcsv/SANTA CRUZ BALANYA.csv 	new file:   outputcsv/SANTA CRUZ DEL QUICHE.csv 	new file:   outputcsv/SANTA MARGARITA.csv 	new file:   outputcsv/SANTA MARIA CAHABON.csv 	new file:   outputcsv/SANTIAGO ATITLAN.csv 	new file:   outputcsv/SUIZA CONTENTA.csv 	new file:   outputcsv/TECUN UMAN.csv 	new file:   outputcsv/TODOS SANTOS.csv 	new file:   outputcsv/TOTONICAPAN.csv

---
+++ consulta_monthly.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+directory="outputcsv/"
+df = pd.read_csv('database.csv', delimiter=',', header=0)
+
+# convertir la columna FECHA a formato datetime y ordenar el dataframe
+df['fecha'] = pd.to_datetime(df['fecha'], format='%d/%m/%Y')
+df = df.sort_values("fecha")
+
+# Obtener la fecha más reciente en el dataframe
+latest_date = df['fecha'].max()
+
+# Obtener el primer día del último mes
+thirty_days_ago = latest_date - pd.Timedelta(days=30)
+
+# Filtrar los datos para que solo incluyan los de los últimos 30 días
+last_30_days_data = df[df['fecha'] >= thirty_days_ago]
+last_30_days_data = last_30_days_data.sort_values("fecha")
+
+last_30_days_data[['Nombre']] = last_30_days_data[['Nombre']].replace('_', ' ', regex=True)
+# Obtener la lista de estaciones disponibles en el dataframe
+estaciones = last_30_days_data["Nombre"].unique()
+
+data=last_30_days_data.groupby(['Nombre'])
+ 
+for estacion in estaciones:
+    # Filtrar los datos para la estación actual
+    data_estacion=data.get_group(estacion)
+    data_estacion.to_csv(f"{directory}{estacion}.csv")
